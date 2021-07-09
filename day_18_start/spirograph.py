1522d4b338131a24a762574baeafd43f86dbd03d
andreybannikov <andrewnnov@yandex.ru>
2021-07-09 09:34:57 +0300

    spirograph_a

--- day_18_start/spirograph.py
+++ day_18_start/spirograph.py
@@ -5,7 +5,7 @@ from random import randint
 
 tim = Turtle()
 t.colormode(255)
-tim.speed("fastest")
+
 
 
 
@@ -13,14 +13,22 @@ def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+    color = (r, g, b)
+    return color
 
 
-for _ in range(360):
-    tim.color(random_color())
-    tim.circle(200)
-    tim.left(1)
+
+tim.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(200)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(1)
 
 
 screen = Screen()
