6a48cf3954016cf8c524b0502fcfb21259e1f312
andreybannikov <andrewnnov@yandex.ru>
2021-08-05 12:20:05 +0300

    create a letter for birthday ang

--- Mail_Merge_Project/main.py
+++ Mail_Merge_Project/main.py
@@ -1,31 +1,14 @@
-
-#Create a letter using starting_letter.txt
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
 
 
-names = open("\\Projects\\100days\\Mail_Merge_Project\\Input\\Names\\invited_names.txt", "r")
-list_of_names = names.readlines()
-print(list_of_names)
-new_list = []
-for el in list_of_names:
-    new_list.append(el.strip("\n"))
-print(new_list)
-
-with open("\\Projects\\100days\\Mail_Merge_Project\\Input\\Letters\\starting_letter.txt", "r") as file:
-    text_from_file = file.read()
-    print(text_from_file)
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
 
 
-for name in new_list:
-    with open(f'\\Projects\\100days\\Mail_Merge_Project\\Output\\ReadyToSend\\starting_letter_{name}.txt', mode='w') as file:
-        file.write(text_from_file.replace("[name]", name))
-        file.close()
-
-
-
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode='w') as completed_letter:
+            completed_letter.write(new_letter)
