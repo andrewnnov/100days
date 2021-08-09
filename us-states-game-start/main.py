82d33772f11e45584268259b425f88f3150de70e
andreybannikov <andrewnnov@yandex.ru>
2021-08-09 06:42:34 +0300

    my example for us quiz

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,4 +1,9 @@
 import turtle
+import pandas
+from turtle import Screen, Turtle
+
+
+FONT = ("Arial", 11, "normal")
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,5 +11,31 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's?")
+write_turtle = Turtle()
+write_turtle.hideturtle()
+write_turtle.penup()
+score = 0
+game_is_on = True
+
+while game_is_on:
+
+    print(score)
+    answer_state = screen.textinput(title=f"{score}/50 Guess the State", prompt="What's another state's?").title()
+
+    state_of_us = pandas.read_csv("50_states.csv")
+    list_of_state = state_of_us["state"].to_list()
+    # print(list_of_state)
+
+
+    if answer_state in list_of_state:
+        score += 1
+        guess_state = state_of_us[state_of_us.state == answer_state]
+        # print(guess_state)
+        x_cor = int(guess_state.x)
+        # print(x_cor)
+        y_cor = int(guess_state.y)
+        # print(y_cor)
+        write_turtle.setpos(x_cor, y_cor)
+        write_turtle.write(answer_state, font=FONT)
+
 
