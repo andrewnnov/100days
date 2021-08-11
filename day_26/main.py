40ebacf7e04174019f0f87be992d12a4e7708183
andreybannikov <andrewnnov@yandex.ru>
2021-08-11 08:05:52 +0300

    connect two lists with common number ang

--- day_26/main.py
+++ day_26/main.py
@@ -1,19 +1,11 @@
 
 
-with open("file1.txt") as file1_data:
-    data_1 = file1_data.readlines()
-    data_int_1 = [int(row.strip("\n")) for row in data_1]
-    print(data_int_1)
+with open("file1.txt") as file1:
+    file1_data = file1.readlines()
 
-with open("file2.txt") as file2_data:
-    data_2 = file2_data.readlines()
-    data_int_2 = [int(row.strip("\n")) for row in data_2]
-    print(data_int_2)
+with open("file1.txt") as file2:
+    file2_data = file2.readlines()
 
 
-if len(data_int_1) > len(data_int_2):
-    result = [number for number in data_int_1 if number in data_int_2]
-else:
-    result = [number for number in data_int_2 if number in data_int_1]
-
+result = [int(num) for num in file1_data if num in file2_data]
 print(result)
