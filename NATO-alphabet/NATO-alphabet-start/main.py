356f06787feb20b5fda473468f6d027ea2deca3d
andreybannikov <andrewnnov@yandex.ru>
2021-08-14 07:42:58 +0300

    nato phonetic alphabet

---
+++ NATO-alphabet/NATO-alphabet-start/main.py
@@ -0,0 +1,19 @@
+import pandas
+
+# Create a dictionary in this format:
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(data)
+
+data_dictionary_alf = {row.letter: row.code for (index, row) in data.iterrows()}
+
+# Create a list of the phonetic code words from a word that the user inputs.
+
+user_word = input("Enter the word: ")
+user_word_cap = user_word.upper()
+
+list_of_letter = []
+for letter in user_word_cap:
+    if letter in data_dictionary_alf.keys():
+        list_of_letter.append(data_dictionary_alf[letter])
+
+print(list_of_letter)
--- day_26/pandas_dictionary.py
+++ day_26/pandas_dictionary.py
@@ -12,7 +12,7 @@ student_dict = {
 
 
 students_data_frame = pandas.DataFrame(student_dict)
-# print(students_data_frame)
+#print(students_data_frame)
 
 
 # loop through a data frame
@@ -22,4 +22,4 @@ for (key, value) in students_data_frame.items():
 # Loop through rows of a data frame
 
 for (index, row) in students_data_frame.iterrows():
-    print(row.student)
+    print(row)
