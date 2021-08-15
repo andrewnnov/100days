dc03e62470c4dbabe3e71b5a2a309e862313d352
andreybannikov <andrewnnov@yandex.ru>
2021-08-15 07:43:56 +0300

    tkinter and *args

---
+++ day_27/main.py
@@ -0,0 +1,25 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first gui program")
+window.minsize(width=500, height=300)
+
+# label
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24))
+my_label.pack(side="left")
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
+
+
+
