a11d6acfa64ce401ec1e6e69397f19788cf0fdbc
andreybannikov <andrewnnov@yandex.ru>
2021-08-15 08:29:43 +0300

    button and imput tkinter

--- day_27/main.py
+++ day_27/main.py
@@ -6,7 +6,39 @@ window.minsize(width=500, height=300)
 
 # label
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24))
-my_label.pack(side="left")
+my_label.pack()
+
+
+my_label["text"] = "New text"
+my_label.config(text="New Text")
+
+# button
+
+
+# def button_clicked():
+#     print("I got clicked")
+#     my_label.config(text="Button Got Clicked")
+#
+#
+# button = tkinter.Button(text="Click me", command=button_clicked)
+# button.pack()
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
+
+# Entry
+input = tkinter.Entry(width=20)
+input.pack()
+
+
+
+
 
 
 
