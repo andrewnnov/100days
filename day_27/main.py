b86f68020b8847e83a58238125befacc79adc59a
andreybannikov <andrewnnov@yandex.ru>
2021-08-16 08:28:57 +0300

    converter miles to km tkinter

--- day_27/main.py
+++ day_27/main.py
@@ -3,11 +3,13 @@ import tkinter
 window = tkinter.Tk()
 window.title("My first gui program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
 
 # label
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24))
-my_label.pack()
-
+my_label.grid(row=0, column=0)
+my_label.config(padx=50, pady=50)
 
 my_label["text"] = "New text"
 my_label.config(text="New Text")
@@ -30,11 +32,16 @@ def button_clicked():
 
 
 button = tkinter.Button(text="Click me", command=button_clicked)
-button.pack()
+button.grid(row=1, column=1)
+
+
+button2 = tkinter.Button(text="Click me too", command=button_clicked)
+button2.grid(row=0, column=2)
+
 
 # Entry
 input = tkinter.Entry(width=20)
-input.pack()
+input.grid(row=2, column=3)
 
 
 
