4cc6c9d9f08cf7b8d3479276126d8542d1280205
andreybannikov <andrewnnov@yandex.ru>
2021-08-25 07:15:17 +0300

    add exception handling to project

--- NATO-alphabet/NATO-alphabet-start/main.py
+++ NATO-alphabet/NATO-alphabet-start/main.py
@@ -8,8 +8,18 @@ data_dictionary_alf = {row.letter: row.code for (index, row) in data.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
 
-user_word = input("Enter a word: ")
-user_word_cap = user_word.upper()
 
-list_of_letter = [data_dictionary_alf[letter] for letter in user_word_cap]
-print(list_of_letter)
+def generate_phonetic():
+    user_word = input("Enter a word: ")
+    user_word_cap = user_word.upper()
+
+    try:
+        list_of_letter = [data_dictionary_alf[letter] for letter in user_word_cap]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(list_of_letter)
+
+
+generate_phonetic()
