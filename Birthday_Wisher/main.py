94ab6416fb90544ba979834bdc2ea1181843535e
andreybannikov <andrewnnov@yandex.ru>
2021-08-31 08:53:21 +0300

    quotes sender

--- Birthday_Wisher/main.py
+++ Birthday_Wisher/main.py
@@ -1,27 +1,24 @@
 import smtplib
 import datetime as dt
-
-
-my_email = "xxxxxx"
-password = "xxxxxx"
-
-
-with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="xxxxxxxxxx",
-                        msg="Subject:Hello\n\nThis is the body")
+import random
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-print(day_of_week)
-ms = now.microsecond
-print(now)
-print(year)
-print(ms)
+weekday = now.weekday()
+print(weekday)
 
-date_of_bith = dt.datetime(year=1952, month=12, day=22, hour=4)
-print(date_of_bith)
+my_email = ""
+password = ""
+
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+        print(quote)
+        with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=my_email,
+                msg=f"Subject: Motivation quote\n\n{quote}")
+
