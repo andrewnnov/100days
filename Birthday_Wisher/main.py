ff7d4153b929a5203af68c78558e5324d8a18158
andreybannikov <andrewnnov@yandex.ru>
2021-09-01 22:17:19 +0300

    draft to birthday

--- Birthday_Wisher/main.py
+++ Birthday_Wisher/main.py
@@ -1,24 +1,13 @@
-import smtplib
-import datetime as dt
-import random
+##################### Extra Hard Starting Project ######################
 
-now = dt.datetime.now()
-weekday = now.weekday()
-print(weekday)
+# 1. Update the birthdays.csv
 
-my_email = ""
-password = ""
+# 2. Check if today matches a birthday in the birthdays.csv
 
-if weekday == 1:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
-        print(quote)
-        with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs=my_email,
-                msg=f"Subject: Motivation quote\n\n{quote}")
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
 
