27b3d2792bbd14762bf4c622497033afee76c415
andreybannikov <andrewnnov@yandex.ru>
2021-09-02 21:33:11 +0300

    send mail

--- Birthday_Wisher/main.py
+++ Birthday_Wisher/main.py
@@ -1,13 +1,29 @@
-##################### Extra Hard Starting Project ######################
+import datetime as dt
+import pandas
+import random
+import smtplib
 
-# 1. Update the birthdays.csv
+my_email = "xxx"
+password = "xxx"
 
-# 2. Check if today matches a birthday in the birthdays.csv
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+data = pandas.read_csv("birthdays.csv")
 
-# 4. Send the letter generated in step 3 to that person's email address.
+birthdays_dict = {(data_row['month'], data_row['day']): data_row for (index, data_row) in data.iterrows()}
 
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-
-
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday\n\n{contents}")
