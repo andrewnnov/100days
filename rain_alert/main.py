deb05e98bc5a066ed5fdbc36c5e9b87af89860d9
andreybannikov <andrewnnov@yandex.ru>
2021-09-14 22:27:47 +0300

    use api for open weather site

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = ""
+api_key = "3d5b1dfa763a3d3d1c506be6f72d3cc3"
 
 MY_LAT = 56.326790
 MY_LONG = 44.005989
@@ -11,14 +11,25 @@ OWM_Endpoint = f"https://api.openweathermap.org/data/2.5/onecall"
 weather_params = {
     "lat": MY_LAT,
     "lon": MY_LONG,
-    "appid": api_key
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
 response = requests.get(url=OWM_Endpoint, params=weather_params)
 response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data['hourly'][:12]
 
-data = response.json()
-print(data)
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data['weather'][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+
 
 
 
